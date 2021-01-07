5adba84b68e8f89f7b0bd64ad236c323f0349fe4
Naruto <umairapi1995@gmail.com>
2021-01-08 00:28:29 +0500

    Configuration Changed

--- core/__init__.py
+++ core/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from core.Config import Config
 import os
 
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///note.db"
+app.config.from_object(Config)
+# app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///note.db"
 
 bcrypt=Bcrypt(app)
 db=SQLAlchemy(app)
