42aa3b4c47c8da9bf6c12d54842f8733fb76ffad
Naruto <umairapi1995@gmail.com>
2021-01-10 19:27:49 +0500

    add small feature to set SECRET_KEY environment variable

--- core/Config.py
+++ core/Config.py
@@ -1,4 +1,5 @@
-import os
+import os, secrets
+os.environ["SECRET_KEY"] = secrets.token_hex(16)
 class Config:
 	SECRET_KEY = os.environ.get("SECRET_KEY")
 	SQLALCHEMY_DATABASE_URI = "sqlite:///note.db"
