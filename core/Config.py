ef586a10a2ce250f22007403450a0e1c9b2dd0fe
Naruto <umairapi1995@gmail.com>
2021-01-11 19:31:02 +0500

    add Mail server configuration

--- core/Config.py
+++ core/Config.py
@@ -1,5 +1,13 @@
 import os, secrets
-os.environ["SECRET_KEY"] = secrets.token_hex(16)
 class Config:
-	SECRET_KEY = os.environ.get("SECRET_KEY")
+	
+	SECRET_KEY = secrets.token_hex(16)
 	SQLALCHEMY_DATABASE_URI = "sqlite:///note.db"
+	# configuration for Mail server
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	MAIL_PORT = 465
+	MAIL_SERVER = 'smtp.gmail.com'
+	MAIL_USE_SSL = True
+	MAIL_USE_TLS = False
+
