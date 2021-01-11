4c2c6e76d4bb2572d5deb1b462e3fd76a7dc2073
Naruto <umairapi1995@gmail.com>
2021-01-11 20:59:37 +0500

    Added functions for encrypt and check password - for resue

--- core/utils/functions.py
+++ core/utils/functions.py
@@ -1,9 +1,19 @@
-from core import mail
+from core import mail,bcrypt
 from flask_mail import Message
 
 
+def is_equal(password,confirm_password):
+	'''
+		Function to check if password and confirm password are equal
+	'''
+	return password==confirm_password
 
 
+def encrypt_password(password):
+	return bcrypt.generate_password_hash(password).decode('utf-8')
+
+def check_password(user_password,password):
+	return bcrypt.check_password_hash(user_password, password)
 def send_mail(url,email):
 	msg = Message("Reset Password",
 		sender='noreply@noter.com',
