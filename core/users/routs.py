e252f63f06c193df823748d1089bdf48ab253e8e
Naruto <umairapi1995@gmail.com>
2021-01-12 02:44:31 +0500

    replace repeated code with functions

--- core/users/routs.py
+++ core/users/routs.py
@@ -1,20 +1,20 @@
 from datetime import datetime
 from flask import (
-	render_template,
-	redirect,
-	url_for,
-	request,
+	render_template,redirect,
+	url_for,request,
 	Blueprint)
 
 from flask_login import (
-	current_user,
-	login_user,
-	login_required,
-	logout_user)
+	current_user,login_user,
+	login_required,logout_user)
+
+
+from core.utils.functions import (
+	send_mail,encrypt_password,check_password,is_equal,register_user,get_user,
+	user_exit_by_username,user_exit_by_email,update_user_data,get_notes)
 
 from core.models import Users,Notes
-from core.utils.functions import send_mail,encrypt_password,check_password,is_equal
-from core import db,bcrypt
+from core import db
 
 noter_users=Blueprint('users',__name__)
 
@@ -30,12 +30,11 @@ def login():
 	if request.method == "POST":
 		email = request.form["email"]
 		password = request.form["password"]
-		user = Users.query.filter_by(email=email).first()
+		user = get_user(email)
 		if user:
 			if check_password(user.password,password):
 				login_user(user)
-				user.last_login_dt=datetime.utcnow()
-				db.session.commit()
+				update_user_data(user,'last_login_dt',datetime.now())
 				return redirect('notes')
 			else: error['password'] = True
 		else:
@@ -58,16 +57,13 @@ def register():
 		password = request.form["password"]
 		c_password= request.form["confirm_password"]
 
-		user_with_username = Users.query.filter_by(username=username).first()
-		user_with_email = Users.query.filter_by(email=email).first()
+		error['email']= user_exit_by_email(email)
+		error['username']=user_exit_by_username(username)
+		error['password'] = not is_equal(password,c_password)
 
-		if user_with_email: error['email']= True
-		if user_with_username: error['username']=True
-		if not is_equal(password,c_password): error['password'] = True
 		if not error['email'] and not error['username'] and not error['password']:
 			user = Users(username=username,email=email,password=encrypt_password(password))
-			db.session.add(user)
-			db.session.commit()
+			register_user(user)
 			return redirect(url_for('users.login'))
 	return render_template("register.html",title= "Register - Noter",error=error)
 
@@ -76,7 +72,7 @@ def register():
 def profile():
 	if not current_user.is_authenticated:
 		return redirect(url_for('users.login'))
-	notes = Notes.query.filter_by(user_id=current_user.id).order_by(Notes.cd.desc())
+	notes = get_notes(current_user.id)
 	numberOfNotes=notes.count()
 	return render_template("profile.html",title = "Profile - Noter",number_of_notes=numberOfNotes)
 
@@ -92,12 +88,12 @@ def reset_password_request():
 	}
 	if request.method == "POST":
 		email=request.form['email']
-		user = Users.query.filter_by(email=email).first()
+		user = get_user(email)
 		if user:
 			token = user.gen_token()
-			data['link'] = f"{url_for('users.reset_password',token=token,_external=True)}"
+			url = data['link'] = f"{url_for('users.reset_password',token=token,_external=True)}"
 			data['email'] = email
-			send_mail(data['link'],data['email'])
+			send_mail(url,email)
 			return render_template('msg.html',title= "Reset Password",email=data['email'])
 		else: error["email"]=True
 	return render_template('reset_password_request.html',title="Reset Password Request",error=error,data=data)
@@ -113,10 +109,10 @@ def reset_password(token):
 	if user is None:
 		error["invalid"]=True
 	if request.method== "POST" and user is not None:
-		if request.form['password'] == request.form["confirm_password"]:
-			password = request.form['password']
-			user.password = encrypt_password(password)
-			db.session.commit()
+		password = request.form['password']
+		c_password = request.form['confirm_password']
+		if is_equal(password,c_password):
+			update_user_data(user,'password',password)
 			return redirect(url_for('users.login'))
 		else: 
 			error["password"]=True
@@ -125,16 +121,16 @@ def reset_password(token):
 # route for change user password
 @noter_users.route('/change_password',methods=["GET","POST"])
 def change_password():
+	if not current_user.is_authenticated:
+		return redirect(url_for('users.login'))
 	error={
 	'password':False
 	}
-	user = Users.query.filter_by(id=current_user.id).first()
 	if request.method== "POST":
 		password = request.form['password']
 		c_password = request.form['confirm_password']
 		if is_equal(password,c_password):
-			user.password = encrypt_password(password)
-			db.session.commit()
+			update_user_data(current_user,'password',password)
 			return redirect(url_for('users.profile'))
 		else: error['password']=True
 	return render_template("change_password.html",title="Change Password" ,error=error)
