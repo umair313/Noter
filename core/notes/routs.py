bea92785aea964be1783e489b1708b7f4f4ff1ea
Naruto <umairapi1995@gmail.com>
2021-01-12 20:17:27 +0500

    comon fix with notes functions and routes and add docs in functions

--- core/notes/routs.py
+++ core/notes/routs.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import render_template ,url_for,redirect,request,Blueprint
 from flask_login import current_user,login_required
+from core.utils.functions import get_all_notes,add_note,del_note,get_note,update_note
 from core.models import Notes
 from core import db
 
@@ -9,7 +10,7 @@ noter_notes= Blueprint('notes',__name__)
 @noter_notes.route('/notes')
 @login_required
 def notes():
-	notes = Notes.query.filter_by(user_id=current_user.id).order_by(Notes.cd.desc())
+	notes = get_all_notes(current_user.id)
 	number_of_notes = notes.count()
 	return render_template("notes.html",title = "All Notes - Noter",notes=notes,count=number_of_notes)
 
@@ -19,37 +20,33 @@ def new_note():
 	if request.method== "POST":
 		title = request.form["notetitle"].replace(" ","-")
 		content=request.form["notecontent"]
-
-		note = Notes(title=title,content=content,user_id=current_user.id)
-		db.session.add(note)
-		db.session.commit()
+		add_note(title,content,current_user.id)
 		return redirect(url_for('notes.notes'))
 	return render_template("new_note.html",title = "Create New Note - Noter")
 
 @noter_notes.route("/note/delete/<int:id>/<string:title>",methods=["POST","GET"])
+@login_required
 def delete_note(id,title):
-	note=Notes.query.filter_by(id=id,title=title,user_id=current_user.id).first()
-	if note:
-		db.session.delete(note)
-		db.session.commit()
+	del_note(id,title,current_user.id)
 	return redirect(url_for("notes.notes"))
 
 @noter_notes.route('/note/view/<int:id>/<string:title>',methods=["POST","GET"])
+@login_required
 def view_note(id,title):
-	note=Notes.query.filter_by(id=id,title=title,user_id=current_user.id).first()
+	note=get_note(id,title,current_user.id)
 	if not note:
 		return redirect(url_for('notes.notes'))
 	return render_template('view_note.html',title = note.title,note=note)
 
 @noter_notes.route('/note/edit/<int:id>/<string:title>',methods=["POST","GET"])
+@login_required
 def edit_note(id,title):
-	note=Notes.query.filter_by(id=id,title=title,user_id=current_user.id).first()
+	note=get_note(id,title,current_user.id)
 	if not note:
 		return redirect(url_for('notes.notes'))
 	elif request.method=="POST":
-		note.title = request.form["notetitle"].replace(' ','-')
-		note.content=request.form["notecontent"]
-		note.md=datetime.utcnow()
-		db.session.commit()
+		title = request.form["notetitle"].replace(' ','-')
+		content=request.form["notecontent"]
+		update_note(note,title,content)
 		return redirect(url_for('notes.notes'))
 	return render_template("edit_note.html",title=note.title,note=note)
--- core/utils/functions.py
+++ core/utils/functions.py
@@ -30,8 +30,6 @@ def get_user(email):
 	'''
 	return Users.query.filter_by(email=email).first()
 
-def get_notes(id):
-	return Notes.query.filter_by(user_id=id).order_by(Notes.cd.desc())
 
 def update_user_data(user,paramiter,value):
 	if paramiter == 'last_login_dt':
@@ -42,9 +40,49 @@ def update_user_data(user,paramiter,value):
 		db.session.commit()
 	return
 
+# ############################################################
+#  Function related to notes                                 
+# ############################################################
 
+def get_all_notes(id):
+	'''
+		It will return Notes matched with given id will return Notes Object
+	'''
+	return Notes.query.filter_by(user_id=id).order_by(Notes.cd.desc())
 
+def add_note(title,content,user_id):
+	'''
+	This function will add new Note into database
+	'''
+	note = Notes(title=title,content=content,user_id=user_id)
+	db.session.add(note)
+	db.session.commit()
+	return
 
+def del_note(note_id :int,title :str,user_id:int):
+	'''
+	This Function will delete note from data base
+	'''
+	note=Notes.query.filter_by(id=note_id,title=title,user_id=user_id).first()
+	if note:
+		db.session.delete(note)
+		db.session.commit()
+	return
+
+def get_note(note_id,title,user_id):
+	'''
+		Return note if found with provided note id,title and user_id othersiwer
+		return None 
+	'''
+	note=Notes.query.filter_by(id=note_id,title=title,user_id=user_id).first()
+	return note
+
+def update_note(note,title,content):
+	note.title = title
+	note.content= content
+	note.md = datetime.now()
+	db.session.commit()
+###############################################################
 def encrypt_password(password):
 	return bcrypt.generate_password_hash(password).decode('utf-8')
 
